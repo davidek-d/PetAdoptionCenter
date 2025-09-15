4443c1cc236c009395aa4bb47a94116b8d77d46a
davidek-d <dajo1002@stud.ek.dk>
2025-09-15 14:13:15 +0200

    firstcommit

--- src/PetCenter/AnimalTest.java
+++ src/PetCenter/AnimalTest.java
@@ -1,4 +1,30 @@
 package PetCenter;
 
+import java.util.Scanner;
+
 public class AnimalTest {
+
+    public static void main(String[] args) {
+
+        getUserInput();
+        displayPets();
+    }
+
+
+    public String welcomeMessage() {
+
+        String welcomeMessage = "Welcome to the adoptive center!";
+        System.out.println(welcomeMessage);
+    }
+
+    public static void getUserInput() {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+
+    public static void displayPets() {
+
+    }
+
 }
