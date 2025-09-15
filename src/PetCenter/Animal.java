f48135475343da2f88531ddf061fd6f7851cb0de
Namirah0310 <nare0001@stud.ek.dk>
2025-09-15 13:50:59 +0200

    commit02

--- src/PetCenter/Animal.java
+++ src/PetCenter/Animal.java
@@ -1,6 +1,20 @@
 package PetCenter;
 
 public class Animal {
-    private String type;
-    private String name;
+    protected String type;
+    protected String name;
+
+    public Animal(String type, String name){
+        this.type = type;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal type: " + type + ", pet name: " + name + ".";
+    }
+
+    public void makeSound(){
+        System.out.println(name + " makes a sound to get your attention.");
+    }
 }
